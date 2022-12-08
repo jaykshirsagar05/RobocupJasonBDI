eed49bb6bb2d1bd4ce788c87d4a4ff2fb2d8597c
yashpatels <yash109916@gmail.com>
2022-12-08 14:10:47 -0500

    Minor fix for bat file

--- Krislet/MyTeam.bat
+++ Krislet/MyTeam.bat
@@ -6,5 +6,5 @@ start java -cp .;jason-2.3.jar Krislet -team Group2 -playerType attacker2
 ping localhost
 start java -cp .;jason-2.3.jar Krislet -team Group2 -playerType defender1
 ping localhost
-start java -cp .;jason-2.3.jar Krislet -team Group2 -playerType attacker2
+start java -cp .;jason-2.3.jar Krislet -team Group2 -playerType defender2
 ping localhost
